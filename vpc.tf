04f7c2db301deb572d6db03510cc1746520e185d
Yuvan-TheDevopsEngineer <149263863+Yuvan-TheDevopsEngineer@users.noreply.github.com>
2023-11-14 14:16:11 +0530

    Terraform Demo Project

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr" {
+}
+variable "vpc_name" {
+}
+
+resource "aws_vpc" "terraform_vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = var.vpc_name
+  }
+}
+output "vpc_id" {
+  value = aws_vpc.terraform_vpc.id
+}
