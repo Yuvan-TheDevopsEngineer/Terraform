fdeeb1f11551c0d7d5c7344492a3ef07bf2bc29a
Yuvan-TheDevopsEngineer <149263863+Yuvan-TheDevopsEngineer@users.noreply.github.com>
2023-11-14 14:26:29 +0530

    Add files via upload

---
+++ terraform/ec2.tf
@@ -0,0 +1,28 @@
+variable "ami_name" {
+}
+variable "instance_type" {
+}
+
+data "aws_ami" "Devops-Image" {
+  most_recent = true
+  owners = ["self"]
+
+  filter {
+    name   = "tags:Name"
+    values = [var.ami_name]
+  }
+  depends_on = [ aws_instance.terraform_ec2 ]
+}
+
+resource "aws_instance" "terraform_ec2" {
+  ami           = data.aws_ami.Devops-Image.id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.terraform_subnet.id
+  tags = {
+    Name = "terraform-instance"
+  }
+}
+
+output "instance_Id" {
+  value = aws_instance.terraform_ec2.id
+}
