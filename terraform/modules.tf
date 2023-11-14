b6fe281e8ab30d2378809c5663e938e6d4ca4c1a
Yuvan-TheDevopsEngineer <149263863+Yuvan-TheDevopsEngineer@users.noreply.github.com>
2023-11-14 15:27:25 +0530

    Add files via upload

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -5,9 +5,6 @@ module "Network-module" {
 vpc_name = "terraform_vpc"
 subnet_cidr = "10.0.0.0/24"
 subnet_name = "terraform_subnet"
-}
-module "Resource-module" {
-  source = "./Resource-module"
   ami_name = "Devops-Image"
 instance_type = "t2.micro"
 }
